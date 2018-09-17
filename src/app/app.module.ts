065e2e648d975d948138d6d9fc4e5c06e98df289
Fernando Arellano <fernando_arellano@epam.com>
2018-09-17 00:59:29 -0500

    Practice 1. Effects Example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './components/article.component';
+import { reducers, metaReducers } from './reducers/reducer';
+import { ArticleEffects } from './effects/article.effects';
+import { ArticleService } from './services/article.service';
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {TestData} from './test-data';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    StoreModule.forRoot(reducers, {metaReducers}),
+    EffectsModule.forRoot([ArticleEffects]),
+    // Test Data will be returned when calling the service.
+    InMemoryWebApiModule.forRoot(TestData)
   ],
-  providers: [],
+  providers: [ ArticleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
